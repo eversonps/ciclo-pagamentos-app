2a4b4abc221e5b15fbd323fd1d5f1c9b48a8c49f
Everson <eversonpereiraofc@gmail.com>
2022-03-14 17:53:13 -0300

    criando o componente menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,13 @@
 import "../common/template/dependencies"
 import React from "react";
 import Header from "../common/template/header";
+import SideBar from "../common/template/sideBar";
 
 export default props => {
     return (
         <div className="wrapper">
             <Header></Header>
+            <SideBar></SideBar>
         </div>
     )
 }
